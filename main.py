5d7d7d61d93418f4ec463565ef824393911aff03
Peter Nedergaard <peter@nedergaard.org>
2022-11-19 00:41:37 +0100

    csv display fix

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import modules.dataFromSoup as Data
 import modules.writeToCSV as Write
 import pandas as pd
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 data = pd.read_csv('wine_data.csv', encoding='utf-8')
-
 print(data)
