1bf1aa519f8603e71171da4c8582a4e7abcf3d68
Peter Nedergaard <peter@nedergaard.org>
2022-11-21 14:05:00 +0100

    save df in csv

--- modules/wineML.py
+++ modules/wineML.py
@@ -11,7 +11,7 @@ lab_enc = preprocessing.LabelEncoder()
 
 data = pd.read_csv('../wine_data.csv', encoding='utf-8')
 
-data_test = data.head(200)
+data_test = data
 data_test = pd.get_dummies(data_test, columns=['country', 'region', 'winery'])
 
 data_test['rating'] = lab_enc.fit_transform(data_test['rating'])
@@ -30,9 +30,9 @@ for row in data_test['grapes']:
 
 onehot_grapes = []
 
-
+i = 1
 for grape in unique_grapes:
-    print(grape)
+    print(str(i) + ': ' + grape + ' of ' + str(len(unique_grapes)))
     grape_list = []
 
     for index in range(len(data_test)):
@@ -42,6 +42,7 @@ for grape in unique_grapes:
             grape_list.append(0)
 
     onehot_grapes.append(grape_list)
+    i += 1
 
 
 for index in range(len(onehot_grapes)):
@@ -53,21 +54,25 @@ for index in range(len(onehot_grapes)):
 
     data_test = data_temp
 
+data_test = data_test.drop('grapes', axis='columns')
 
-model = DecisionTreeClassifier()
+data_test.to_csv('../wine_data_prepared.csv', encoding='utf-8', index=False)
+# data_test = pd.read_csv('../wine_data_prepared.csv', encoding='utf-8')
 
-X = data_test.drop(['rating', 'grapes'], axis='columns').values
+# model = DecisionTreeClassifier()
+#
+# X = data_test.drop(['rating'], axis='columns').values
+#
+# y = data_test['rating'].values
+#
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+#
+# model.fit(X_train, y_train)
 
-y = data_test['rating'].values
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
-
-model.fit(X_train, y_train)
-
-pickle.dump(model, open('wine_model.sav', 'wb'))
+# pickle.dump(model, open('../wine_model.sav', 'wb'))
 
 # model = pickle.load(open('wine_model.sav', 'rb'))
-#
+
 # predictions = model.predict(X_test)
 # score = accuracy_score(y_test, predictions)
 #
